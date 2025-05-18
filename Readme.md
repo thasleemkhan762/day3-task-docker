807c6ac94cb634a27369746b4b8e4dad7f0dfa36
jishnumonP <jishnup981@gmail.com>
2025-05-18 22:12:02 +0530

    Day 3 initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# 🚀 Simple Dockerized Node.js Express App - Day 3
+
+This is a beginner-friendly Node.js project using Express. It is fully Dockerized so you can run it in any environment without worrying about setup.
+
+---
---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Hello from Dockerized Node.js Express Server!');
+});
+
+app.listen(PORT, () => {
+    console.log(`✅ App is running`);
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});

e06aa67b9fdb77bcb7e30a8443c3b5aa8e593d8f
Thasleem-tlechnologies <thasleemkhan762@gmail.com>
2025-07-13 17:36:25 +0530

    Update server greeting message and modify package-lock.json for project renaming and dependency updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from Dockerized Node.js Express Server!');
+  res.send('🚀 Hello from Dockerized Node.js Express Server! updated');
 });
 
 app.listen(PORT, () => {
